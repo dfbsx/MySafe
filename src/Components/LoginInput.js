38e03ded3092a470bcff264c12773995b3783f62
Dominika <werkamdor@gmail.com>
2023-04-02 18:10:13 +0200

    Login view ready

---
+++ src/Components/LoginInput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LoginInput.css';
+
+function LoginInput({inputText}) {
+  return (
+    <div><input placeholder={inputText}/></div>
+  )
+}
+
+export default LoginInput
--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from '../Components/Header';
 import './LoginView.css';
+import LoginInput from '../Components/LoginInput';
 
 function LoginView() {
   return (
     <div className='loginView'>
         <Header/>
-        <div className="site">
+        <div className="loginSite">
+            <form className="loginForm">
+                <p>Zaloguj się</p>
+                <LoginInput inputText="Login"/>
+                <LoginInput inputText="Hasło"/>
+                <button>ZALOGUJ</button>
+            </form>
         </div>
     </div>
   )
